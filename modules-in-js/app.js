7d091091aeb6d0e1f5c15982d8c6d7026dd07478
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-07-04 14:30:58 -0300

    Configuring Webpack

---
+++ modules-in-js/app.js
@@ -0,0 +1,6 @@
+import * as R from 'ramda';
+
+const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
+const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
+
+console.log(R.union(arr1, arr2));
---
+++ modules-in-js/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'development',
+  entry: {
+    filename: './app.js',
+  },
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+          },
+        },
+      },
+    ],
+  },
+};
