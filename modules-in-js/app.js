58c9eb96053d3ba237e6568906ffc38084b237f3
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-07-31 13:33:45 -0300

    Initializing a server with Webpack

--- modules-in-js/app.js
+++ modules-in-js/app.js
@@ -4,7 +4,7 @@ import React from 'react';
 import reactDOM from 'react-dom';
 
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
-const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 1];
+const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, 11];
 
-console.log(R.uni(arr1, arr2));
+console.log(R.union(arr1, arr2));
 
