64d59c86333d258e7772d2105667731b359a5d36
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-08-08 21:39:59 -0300

    Mocha operation - describe, context, it ...

---
+++ tests/main.spec.js
@@ -0,0 +1,13 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen bla bla bla', () => {
+        // espera que aconteça
+        // Entrada de dados / método sum(2, 2)
+        // Espera retornar (4) => true |  (3) => false
+      });
+    });
+    context('Case 2', () => {});
+  });
+  describe('Method B', () => {});
+});
