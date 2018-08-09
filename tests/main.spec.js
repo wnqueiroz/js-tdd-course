3280d90139288e25f4966431944eb4fa518ab4b6
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-08-08 21:50:16 -0300

    Commands Skip, Only, Bail

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,13 +1,21 @@
 describe('Main', () => {
   describe('Method A', () => {
     context('Case 1', () => {
+      it.skip('should happen bla bla bla', () => {
+        // espera que aconteça
+        // Entrada de dados / método sum(2, 2)
+        // Espera retornar (4) => true |  (3) => false
+        throw new Error('just an error');
+      });
+    });
+    context('Case 2', () => {
       it('should happen bla bla bla', () => {
         // espera que aconteça
         // Entrada de dados / método sum(2, 2)
         // Espera retornar (4) => true |  (3) => false
+        throw new Error('just another error');
       });
     });
-    context('Case 2', () => {});
   });
   describe('Method B', () => {});
 });
