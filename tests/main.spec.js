31af510c232139f94696a794e4df7a9f5ff5f288
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-09-05 10:54:16 -0300

    Hooks in practice

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,25 +1,37 @@
 describe('Main', () => {
+  let array = [];
+
   // roda uma vez antes do bloco
   before(() => {
-    console.log('before');
+
   });
 
   // roda uma vez depois do bloco
   after(() => {
-    console.log('after');
+
   });
 
   // roda todas as vezes, antes de CADA bloco
   beforeEach(() => {
-    console.log('beforeEach');
+    array = [1, 2, 3]
   });
 
   // roda todas as vezes, depois de CADA bloco
   afterEach(() => {
-    console.log('afterEach');
+
   });
 
-  it('test 1', () => {});
+  it('should have a size of 4 when push another value to the array', () => {
+    array.push(4)
+    console.log(array.length)
+  });
 
-  it('test 2', () => {});
+  it('should a size of 2 when pop a value from the array', () => {
+    array.pop()
+    console.log(array.length)
+  });
+
+  it('should remove the value 3 when use pop in the array', () => {
+    console.log(array.pop() === 3) // true
+  });
 });
