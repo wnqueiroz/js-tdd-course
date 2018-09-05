2a9cc1e196a31f6b1618db6fc9e13a99b2a3a0bc
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-09-05 19:37:15 -0300

    Methods from Chai

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,37 +1,35 @@
+var expect = require('chai').expect
+
 describe('Main', () => {
   let array = [];
-
-  // roda uma vez antes do bloco
-  before(() => {
-
-  });
-
-  // roda uma vez depois do bloco
-  after(() => {
-
-  });
 
   // roda todas as vezes, antes de CADA bloco
   beforeEach(() => {
     array = [1, 2, 3]
   });
 
-  // roda todas as vezes, depois de CADA bloco
-  afterEach(() => {
-
-  });
+  // testar tipos ou se existe (smoke test)
+  it('should be an array', () => {
+    expect(array).to.be.an('array')
+  })
 
   it('should have a size of 4 when push another value to the array', () => {
     array.push(4)
-    console.log(array.length)
+    expect(array).to.have.lengthOf(4);
+  });
+
+  it('should return true when pop 3 from the array', () => {
+    expect(array.pop() === 3).to.be.true
   });
 
   it('should a size of 2 when pop a value from the array', () => {
     array.pop()
-    console.log(array.length)
+    expect(array).to.have.lengthOf(2);
   });
 
   it('should remove the value 3 when use pop in the array', () => {
-    console.log(array.pop() === 3) // true
+    array.pop() === 3
+    expect(array).to.not.include(3)
   });
+
 });
