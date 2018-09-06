d246a75505917339c5c51bef0079c4824839bbe2
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-09-05 21:41:07 -0300

    Fix: Eslint

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-const FizzBuzz = num => {
-  if (num === 0) return 0
-  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz'
-  if (num % 3 === 0) return 'Fizz'
-  if (num % 5 === 0) return 'Buzz'
-  return num
-}
+const FizzBuzz = (num) => {
+  if (num === 0) return 0;
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
+};
 
-function foo() {
-  return 'bla'
-}
-
-export default FizzBuzz
+export default FizzBuzz;
