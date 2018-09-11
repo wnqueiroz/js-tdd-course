59983b6027b32577d072d06e9154b4a91ddac0f5
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-09-11 16:09:23 -0300

    Fix: ESlint errors

--- src/album.js
+++ src/album.js
@@ -1,8 +1,8 @@
-import { API_URL } from './config';
+import API_URL from './config';
 import toJSON from './utils';
 
 // prettier-ignore
-const getAlbum = id => fetch(`${API_URL}/albums/${id}`).then(toJSON)
+const getAlbum = id => fetch(`${API_URL}/albums/${id}`).then(toJSON);
 
 // prettier-ignore
 const getAlbums = ids => fetch(`${API_URL}/albums/?ids=${ids}`).then(toJSON);
