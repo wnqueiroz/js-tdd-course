6a0bc1b86b67ef786c4b93adc9b0fe533636dd95
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-10-17 19:36:28 -0300

    How Stories Work

--- stories/Main.js
+++ stories/Main.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Component from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
+storiesOf('React Full Header', module)
     .add('with lyef name', () => (
         <Component name="lyef" />
     ))
     .add('with another name', () => (
         <Component name="another" />
     ))
+    .add('with TDD name', () => (
+        <Component name="TDD" />
+    ));
