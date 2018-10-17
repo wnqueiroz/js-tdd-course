169a18fee488db7e0d006a0af8da7e6d048315b7
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-10-17 18:57:17 -0300

    Removed old files

--- src/album.js
+++
@@ -1,7 +0,0 @@
-export default function album() {
-  return {
-    getAlbum: id => this.request(`${this.apiUrl}/albums/${id}`),
-    getAlbums: ids => this.request(`${this.apiUrl}/albums/?ids=${ids}`),
-    getTracks: id => this.request(`${this.apiUrl}/albums/${id}/tracks`)
-  };
-}
--- src/config.js
+++
@@ -1,9 +0,0 @@
-const API_URL = 'https://api.spotify.com/v1';
-const API_TOKEN = 'BQDiht0WegYtdLV2od6UUZOlz4q3zpGtsC8ImN6I4-gFNG0DWexGJ8XTqst0T6BI_pi9_MdwpmLVmtQak80trg4gsGKyep4k3RaHxCO9fW2uZ1uA5OiSF-ACY5A67gyUW_MndhjS-4jg9qGXxeRpJK1Wkxv_jf0F_xRKM98AVbOUN6h9IRtiWWzvv-SK1EzEMEbXc0igOgdlnzQwqOiSdiKTM4xe1CxZBcYg412fNTQZYFyHzYplIvEC3klDJPtG968dGGeGmXMYjf-kfy-6';
-const HEADERS = {
-  headers: {
-    Authorization: `Bearer ${API_TOKEN}`
-  }
-};
-
-export { API_URL, HEADERS };
