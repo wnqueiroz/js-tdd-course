d945f752d0a71643eb9f7b3e11f3388c8551fdb9
William Queiroz <wnqueiroz@outlook.com>
2019-01-22 22:43:06 -0200

    Adding bgColor

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FullHeader = ({ title, subtitle }) => {
+const defaultProps = {
+    bgColor: '#ccc',
+};
+
+const FullHeader = ({ title, subtitle, bgColor: backgroundColor }) => {
+    const styles = {
+        header: {
+            backgroundColor,
+        },
+    };
     const component = (
-        <header>
+        <header style={styles.header} >
             {title && <h1>{title}</h1>}
             {subtitle && <h2>{subtitle}</h2>}
         </header>
@@ -16,5 +25,6 @@ FullHeader.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
 };
+FullHeader.defaultProps = defaultProps;
 
 export default FullHeader;
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiEnzime from 'chai-enzyme';
 import { shallow } from 'enzyme';
 import FullHeader from '../../src/Main';
+
+chai.use(chaiEnzime());
 
 describe('<FullHeader />', () => {
     it('should have header tag when mount', () => {
@@ -40,6 +43,18 @@ describe('<FullHeader />', () => {
             const subtitle = 'TDD';
             const wrapper = shallow(<FullHeader subtitle={subtitle} />);
             expect(wrapper.find('h2').props().children).to.be.equal(subtitle);
+        });
+    });
+
+    context('bgColor', () => {
+        it('should have background-color equal #ccc when none is passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" />);
+            expect(wrapper).to.have.style('background-color').equal('#ccc');
+        });
+        it('should have background-color equal #000 when is passed', () => {
+            const bgColor = '#000';
+            const wrapper = shallow(<FullHeader title="TDD" bgColor={bgColor} />);
+            expect(wrapper).to.have.style('background-color').equal(bgColor);
         });
     });
 });
