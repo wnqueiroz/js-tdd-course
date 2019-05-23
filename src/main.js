9df41663ee3ff1bc0ce747dc9d271cb453128561
William Queiroz <wnqueiroz@outlook.com>
2019-05-23 17:49:15 -0300

    Adding AlbumInfo to Layout

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import spotify from './Spotify';
 
 import { renderAlbums } from './AlbumList';
+import { renderAlbumInfo } from './AlbumInfo';
+
+const albumList = document.getElementById('album-list');
+const albumInfo = document.getElementById('album-info');
 
 const albums = spotify.search.albums('Alter bridge');
 
-const albumList = document.getElementById('album-list');
+const album = spotify.album.getAlbum('6hqietEuZCikevdO2oGTcx');
 
 albums.then(({ albums: { items = [] } = {} }) => renderAlbums(items, albumList));
+
+album.then(data => renderAlbumInfo(data, albumInfo));
