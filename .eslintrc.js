16475fb2e261e47cdf02a810c8d06a0f9c862377
William Queiroz <wnqueiroz@outlook.com>
2019-05-23 18:49:02 -0300

    Fix ESLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
 };
