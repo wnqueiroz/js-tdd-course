32233a2d34ad6aeb17f9580bffbe0ea1712c7eca
William Queiroz <wnqueiroz@outlook.com>
2019-05-23 17:41:28 -0300

    Creating AlbumInfo component

---
+++ src/AlbumInfo.js
@@ -0,0 +1,11 @@
+const renderAlbumInfo = (data, element) => {
+  const markup = `
+  <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
+  <p class="album-title">${data.name}</p>
+  <p class="album-artist">${data.artists[0].name}</p>
+  <p class="album-counter">${data.tracks.total} Músicas</p>`;
+
+  element.innerHTML = markup;
+};
+
+export { renderAlbumInfo };
