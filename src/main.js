6e03d9ccf28032967f19760006209b122fa5871f
William Queiroz <wnqueiroz@outlook.com>
2019-05-23 18:23:11 -0300

    Adding AlbumTracks to Layout

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import spotify from './Spotify';
 
 import { renderAlbums } from './AlbumList';
 import { renderAlbumInfo } from './AlbumInfo';
+import { renderAlbumTracks } from './AlbumTracks';
 
 const albumList = document.getElementById('album-list');
 const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-tracks');
 
 const albums = spotify.search.albums('Alter bridge');
 
@@ -12,4 +14,9 @@ const album = spotify.album.getAlbum('6hqietEuZCikevdO2oGTcx');
 
 albums.then(({ albums: { items = [] } = {} }) => renderAlbums(items, albumList));
 
-album.then(data => renderAlbumInfo(data, albumInfo));
+album
+  .then((data) => {
+    renderAlbumInfo(data, albumInfo);
+    return data;
+  })
+  .then(({ tracks: { items = [] } = {} }) => renderAlbumTracks(items, albumTracks));
