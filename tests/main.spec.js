77a7b033deef08f96ad6e5108a91120e5f20b06c
William Queiroz <wnqueiroz@outlook.com>
2019-05-24 15:56:39 -0300

    Initialized CLI and tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,17 @@
 import { expect } from 'chai';
 
-describe('Main', () => {
-  it('should do what...', () => {
-    expect(true).to.be.true;
+import { exec } from 'child_process';
+
+const isWindows = process.platform === 'win32';
+
+const btcConverter = `${isWindows ? 'node.exe' : ''} ./src/main.js`;
+
+describe('Main CLI', () => {
+  it('should do return Hello World', (done) => {
+    exec(btcConverter, (err, stdout) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World');
+      done();
+    });
   });
 });
